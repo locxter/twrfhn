dc4e2822df7f591b9f309a66721319a66273acf9
locxter <54595101+locxter@users.noreply.github.com>
2021-03-18 16:23:28 +0100

    Styled the whole thing annd improved some code.

--- scripts/index.js
+++ scripts/index.js
@@ -2,34 +2,64 @@
 "use strict";
 
 // Creating constants for DOM interaction
-const INPUT_DISK_COUNT = document.getElementById('disk-count');
-const BUTTON_CALCULATE_MOVES = document.getElementById('calculate-moves');
-const CANVAS_OUTPUT = document.getElementById('output');
-const CANVAS_OUTPUT_CONTEXT = CANVAS_OUTPUT.getContext('2d');
-const BUTTON_PREVIOUS_MOVE = document.getElementById('previous-move');
-const BUTTON_NEXT_MOVE = document.getElementById('next-move');
+const DISK_COUNT = document.getElementById("disk-count");
+const CALCULATE_GAME_STATES = document.getElementById("calculate-game-states");
+const CANVAS = document.getElementById("output");
+const CANVAS_CONTEXT = CANVAS.getContext("2d");
+const PREVIOUS_GAME_STATE = document.getElementById("previous-game-state");
+const NEXT_GAME_STATE = document.getElementById("next-game-state");
 
 // Global variables
-var moves = [];
+var gameStates = [];
 var scalingRatio = 1;
 var diskCount = 0;
-var visibleMove = 0;
+var visibleGameState = 0;
+var colorSchemeDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// A small helper function for resizing the canvas
+function modifyCanvas()
+{
+    // Sizing the canvas to the window width
+    CANVAS.width = document.documentElement.clientWidth * .9;
+    CANVAS.height = document.documentElement.clientWidth * .36;
+    // Updating the scaling ratio to fit the internal resolution of 300x120 to the real canvas size
+    scalingRatio = CANVAS.width / 300;
+    // Setting the right canvas color
+    if (colorSchemeDark)
+    {
+        CANVAS_CONTEXT.fillStyle = "#fff";
+    }
+    else
+    {
+        CANVAS_CONTEXT.fillStyle = "#1b1d20";
+    }
+    // Displaying a small informational text on the canvas
+    CANVAS_CONTEXT.font = (5 * scalingRatio) + "px monospace";
+    CANVAS_CONTEXT.textAlign = "center";
+    CANVAS_CONTEXT.textBaseline = "center";
+    CANVAS_CONTEXT.fillText("Click \"Calculate game states\" to see the visualization.", 150 * scalingRatio, 55 * scalingRatio);
+    // Repainting the canvas when needed
+    if (gameStates.length > 0)
+    {
+        visualizeGameState(visibleGameState);
+    }
+}
 
 // An iterative algorithm to solve the game
-function calculateMoves()
+function calculateGameStates()
 {
     // Creating the rod start configuration
-    let data = [
+    let rods = [
                    [],
                    [],
                    []
                ];
     for (var i = 0; i < diskCount; i++)
     {
-        data[0].push(i);
+        rods[0].push(i);
     }
-    // Resetting the move history
-    moves = [JSON.parse(JSON.stringify(data))];
+    // Resetting the gamestate history
+    gameStates = [JSON.parse(JSON.stringify(rods))];
     // Setting the movement direction for disk 0 following this scheme: [last, current, next]
     let disk0Positions = [];
     if (diskCount % 2 === 0)
@@ -43,28 +73,28 @@ function calculateMoves()
         disk0Positions = [1, 0, 2];
     }
     // Moving disks until all of them are on rod C and in the right order
-    while (data[2].length < diskCount)
+    while (rods[2].length < diskCount)
     {
         // Moving disk 0 to the next rod
-        data[disk0Positions[2]].unshift(0);
-        data[disk0Positions[1]].shift();
+        rods[disk0Positions[2]].unshift(0);
+        rods[disk0Positions[1]].shift();
         // Storing the current move
-        moves.push(JSON.parse(JSON.stringify(data)));
-        if (data[disk0Positions[0]][0] < data[disk0Positions[1]][0] || (data[disk0Positions[1]][0] === undefined && data[2].length < diskCount))
+        gameStates.push(JSON.parse(JSON.stringify(rods)));
+        if (rods[disk0Positions[0]][0] < rods[disk0Positions[1]][0] || (rods[disk0Positions[1]][0] === undefined && rods[2].length < diskCount))
         {
             // Moving another disk from second last to last position of disk 0 if possible
-            data[disk0Positions[1]].unshift(data[disk0Positions[0]][0]);
-            data[disk0Positions[0]].shift();
+            rods[disk0Positions[1]].unshift(rods[disk0Positions[0]][0]);
+            rods[disk0Positions[0]].shift();
             // Storing the current move
-            moves.push(JSON.parse(JSON.stringify(data)));
+            gameStates.push(JSON.parse(JSON.stringify(rods)));
         }
-        else if (data[disk0Positions[1]][0] < data[disk0Positions[0]][0] || (data[disk0Positions[0]][0] === undefined && data[2].length < diskCount))
+        else if (rods[disk0Positions[1]][0] < rods[disk0Positions[0]][0] || (rods[disk0Positions[0]][0] === undefined && rods[2].length < diskCount))
         {
             // Moving another disk from last to second last position of disk 0 if possible
-            data[disk0Positions[0]].unshift(data[disk0Positions[1]][0]);
-            data[disk0Positions[1]].shift();
+            rods[disk0Positions[0]].unshift(rods[disk0Positions[1]][0]);
+            rods[disk0Positions[1]].shift();
             // Storing the current move
-            moves.push(JSON.parse(JSON.stringify(data)));
+            gameStates.push(JSON.parse(JSON.stringify(rods)));
         }
         // Setting new positions of disk 0 for the next iteration
         disk0Positions.push(disk0Positions[0]);
@@ -72,90 +102,95 @@ function calculateMoves()
     }
 }
 
-// A small helper function for resizing the canvas
-function resizeCanvas()
-{
-    // Sizing the canvas to the window width
-    CANVAS_OUTPUT.width = document.documentElement.clientWidth;
-    CANVAS_OUTPUT.height = document.documentElement.clientWidth / (300 / 120);
-    // Updating the scaling ratio to fit the internal resolution of 300 x 110 to the real canvas size
-    scalingRatio = CANVAS_OUTPUT.width / 300;
-    // Displaying a small informational text on the canvas
-    CANVAS_OUTPUT_CONTEXT.font = (5 * scalingRatio) + 'px sans-serif';
-    CANVAS_OUTPUT_CONTEXT.textAlign = 'center';
-    CANVAS_OUTPUT_CONTEXT.textBaseline = 'center';
-    CANVAS_OUTPUT_CONTEXT.fillText('Click "Calculate moves" to see the visualization', 150 * scalingRatio, 55 * scalingRatio);
-    // Repainting the canvas when needed
-    if (moves.length > 0)
-    {
-        visualizeMove(visibleMove);
-    }
-}
-
 // An algorithm to visualize the game states
-function visualizeMove(gameState = 0)
+function visualizeGameState(gameState = 0)
 {
     // Calculating the disk scaling ratio (width difference between different disk)
     let diskScalingRatio = 100 / diskCount;
     // Clearing the canvas
-    CANVAS_OUTPUT_CONTEXT.clearRect(0, 0, CANVAS_OUTPUT.width, CANVAS_OUTPUT.height);
-    // Drawing game state label
-    CANVAS_OUTPUT_CONTEXT.font = (10 * scalingRatio) + 'px sans-serif';
-    CANVAS_OUTPUT_CONTEXT.fillText(gameState + '/' + (moves.length - 1), 150 * scalingRatio, 9 * scalingRatio);
+    CANVAS_CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    // Drawing the game state label
+    CANVAS_CONTEXT.font = (10 * scalingRatio) + "px monospace";
+    CANVAS_CONTEXT.fillText(gameState + "/" + (gameStates.length - 1), 150 * scalingRatio, 8 * scalingRatio);
     for (var i = 0; i < 3; i++)
     {
         // Drawing a rod
-        CANVAS_OUTPUT_CONTEXT.fillStyle = "#0000ff";
-        CANVAS_OUTPUT_CONTEXT.fillRect((47.5 + (100 * i)) * scalingRatio, 10 * scalingRatio, 5 * scalingRatio, 100 * scalingRatio);
+        CANVAS_CONTEXT.fillRect((47.5 + (100 * i)) * scalingRatio, 10 * scalingRatio, 5 * scalingRatio, 100 * scalingRatio);
         // Drawing the disks on the rod
-        CANVAS_OUTPUT_CONTEXT.fillStyle = "#000000";
-        for (var j = 0; j < moves[gameState][i].length; j++)
+        for (var j = 0; j < gameStates[gameState][i].length; j++)
         {
-            let diskWidth = diskScalingRatio * (moves[gameState][i][j] + 1);
-            CANVAS_OUTPUT_CONTEXT.fillRect((((100 - diskWidth) / 2) + (100 * i)) * scalingRatio, ((111 - (10 * moves[gameState][i].length)) + (10 * j)) * scalingRatio, diskWidth * scalingRatio, 9 * scalingRatio);
+            let diskWidth = diskScalingRatio * (gameStates[gameState][i][j] + 1);
+            CANVAS_CONTEXT.fillRect((((100 - diskWidth) / 2) + (100 * i)) * scalingRatio, ((111 - (10 * gameStates[gameState][i].length)) + (10 * j)) * scalingRatio, diskWidth * scalingRatio, 9 * scalingRatio);
         }
     }
     // Drawing rod labels
-    CANVAS_OUTPUT_CONTEXT.fillText('A', 50 * scalingRatio, 118 * scalingRatio);
-    CANVAS_OUTPUT_CONTEXT.fillText('B', 150 * scalingRatio, 118 * scalingRatio);
-    CANVAS_OUTPUT_CONTEXT.fillText('C', 250 * scalingRatio, 118 * scalingRatio);
+    CANVAS_CONTEXT.fillText("A", 50 * scalingRatio, 118 * scalingRatio);
+    CANVAS_CONTEXT.fillText("B", 150 * scalingRatio, 118 * scalingRatio);
+    CANVAS_CONTEXT.fillText("C", 250 * scalingRatio, 118 * scalingRatio);
 }
 
-// Handling button clicks
-BUTTON_CALCULATE_MOVES.addEventListener('click', function ()
+// Fitting the canvas to the window size
+window.addEventListener("load", function()
 {
-    diskCount = INPUT_DISK_COUNT.value;
-    if (diskCount < 0)
+    modifyCanvas();
+    modifyCanvas();
+});
+window.addEventListener("resize", function()
+{
+    modifyCanvas();
+    modifyCanvas();
+});
+window.addEventListener("rotate", function()
+{
+    modifyCanvas();
+    modifyCanvas();
+});
+
+// Reacting to color preference change
+window.matchMedia("(prefers-color-scheme: dark)").addListener(function(mediaQuery)
+{
+    if (mediaQuery.matches)
     {
-        diskCount = 0;
+        colorSchemeDark = true;
+    }
+    else
+    {
+        colorSchemeDark = false;
+    }
+    modifyCanvas();
+});
+
+// Handling button clicks
+CALCULATE_GAME_STATES.addEventListener("click", function ()
+{
+    diskCount = DISK_COUNT.value;
+    if (diskCount < 1)
+    {
+        diskCount = 1;
     }
     else if (diskCount > 10)
     {
         diskCount = 10;
     }
-    calculateMoves();
-    visibleMove = 0;
-    visualizeMove(visibleMove);
+    calculateGameStates();
+    visibleGameState = 0;
+    visualizeGameState(visibleGameState);
 });
-BUTTON_PREVIOUS_MOVE.addEventListener('click', function ()
-{
-    if (visibleMove > 0)
-    {
-        visibleMove--;
-        visualizeMove(visibleMove);
-    }
-});
-BUTTON_NEXT_MOVE.addEventListener('click', function ()
-{
-    if (visibleMove < moves.length - 1)
-    {
-        visibleMove++;
-        visualizeMove(visibleMove);
-    }
-});
-// Fitting the canvas to the window size
-window.addEventListener('resize', resizeCanvas);
-window.addEventListener('rotate', resizeCanvas);
-resizeCanvas();
-resizeCanvas();
 
+PREVIOUS_GAME_STATE.addEventListener("click", function ()
+{
+    if (visibleGameState > 0)
+    {
+        visibleGameState--;
+        visualizeGameState(visibleGameState);
+    }
+});
+
+NEXT_GAME_STATE.addEventListener("click", function ()
+{
+    if (visibleGameState < gameStates.length - 1)
+    {
+        visibleGameState++;
+        visualizeGameState(visibleGameState);
+    }
+});
