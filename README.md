3515aa8bb192e37456ba1fdc1342012f5a811698
locxter <54595101+locxter@users.noreply.github.com>
2021-04-27 16:36:50 +0200

    Improved the styling a lot.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# locxter's Tower of Hanoi
+# Tower of Hanoi
+
+## Overview
 
 This is my visualization of the classic game Tower of Hanoi using an iterative algorithm to calculate the fastest way of solving it.
+
+## Dependencies
+
+I generally try to minimize dependencies, but I'm a one man crew and can therefore only support Ubuntu based Linux distributions as I'm running Linux Mint myself. Anyway, you need to have the following packages installed for everything to work properly:
+
+- Firefox for viewing the website. Install it with `sudo apt install firefox`.
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // Activating strict mode
 "use strict";
 
-// Creating constants for DOM interaction
+// DOM references
 const DISK_COUNT = document.getElementById("disk-count");
 const CALCULATE_GAME_STATES = document.getElementById("calculate-game-states");
 const CANVAS = document.getElementById("output");
@@ -9,12 +9,13 @@ const CANVAS_CONTEXT = CANVAS.getContext("2d");
 const PREVIOUS_GAME_STATE = document.getElementById("previous-game-state");
 const NEXT_GAME_STATE = document.getElementById("next-game-state");
 
-// Global letiables
+// Global variables
 let gameStates = [];
 let scalingRatio = 1;
 let diskCount = 0;
 let visibleGameState = 0;
-let colorSchemeDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+let textColor = getComputedStyle(document.documentElement).getPropertyValue("--text-color");
+let accentColor = getComputedStyle(document.documentElement).getPropertyValue("--accent-color");
 
 // A small helper function for resizing the canvas
 function modifyCanvas()
@@ -24,17 +25,9 @@ function modifyCanvas()
     CANVAS.height = document.documentElement.clientWidth * .36;
     // Updating the scaling ratio to fit the internal resolution of 300x120 to the real canvas size
     scalingRatio = CANVAS.width / 300;
-    // Setting the right canvas color
-    if (colorSchemeDark)
-    {
-        CANVAS_CONTEXT.fillStyle = "#fff";
-    }
-    else
-    {
-        CANVAS_CONTEXT.fillStyle = "#1b1d20";
-    }
     // Displaying a small informational text on the canvas
-    CANVAS_CONTEXT.font = (5 * scalingRatio) + "px monospace";
+    CANVAS_CONTEXT.fillStyle = textColor;
+    CANVAS_CONTEXT.font = (5 * scalingRatio) + "px sans-serif";
     CANVAS_CONTEXT.textAlign = "center";
     CANVAS_CONTEXT.textBaseline = "center";
     CANVAS_CONTEXT.fillText("Click \"Calculate game states\" to see the visualization.", 150 * scalingRatio, 55 * scalingRatio);
@@ -110,20 +103,24 @@ function visualizeGameState(gameState = 0)
     // Clearing the canvas
     CANVAS_CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
     // Drawing the game state label
-    CANVAS_CONTEXT.font = (10 * scalingRatio) + "px monospace";
+    CANVAS_CONTEXT.fillStyle = textColor;
+    CANVAS_CONTEXT.font = (10 * scalingRatio) + "px sans-serif";
     CANVAS_CONTEXT.fillText(gameState + "/" + (gameStates.length - 1), 150 * scalingRatio, 8 * scalingRatio);
     for (let i = 0; i < 3; i++)
     {
         // Drawing a rod
+        CANVAS_CONTEXT.fillStyle = textColor;
         CANVAS_CONTEXT.fillRect((47.5 + (100 * i)) * scalingRatio, 10 * scalingRatio, 5 * scalingRatio, 100 * scalingRatio);
         // Drawing the disks on the rod
         for (let j = 0; j < gameStates[gameState][i].length; j++)
         {
             let diskWidth = diskScalingRatio * (gameStates[gameState][i][j] + 1);
+            CANVAS_CONTEXT.fillStyle = accentColor;
             CANVAS_CONTEXT.fillRect((((100 - diskWidth) / 2) + (100 * i)) * scalingRatio, ((111 - (10 * gameStates[gameState][i].length)) + (10 * j)) * scalingRatio, diskWidth * scalingRatio, 9 * scalingRatio);
         }
     }
     // Drawing rod labels
+    CANVAS_CONTEXT.fillStyle = textColor;
     CANVAS_CONTEXT.fillText("A", 50 * scalingRatio, 118 * scalingRatio);
     CANVAS_CONTEXT.fillText("B", 150 * scalingRatio, 118 * scalingRatio);
     CANVAS_CONTEXT.fillText("C", 250 * scalingRatio, 118 * scalingRatio);
@@ -149,14 +146,8 @@ addEventListener("rotate", function()
 // Reacting to color preference change
 matchMedia("(prefers-color-scheme: dark)").addListener(function(mediaQuery)
 {
-    if (mediaQuery.matches)
-    {
-        colorSchemeDark = true;
-    }
-    else
-    {
-        colorSchemeDark = false;
-    }
+    textColor = getComputedStyle(document.documentElement).getPropertyValue("--text-color");
+    accentColor = getComputedStyle(document.documentElement).getPropertyValue("--accent-color");
     modifyCanvas();
 });
 
