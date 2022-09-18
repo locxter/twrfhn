ec979423d5746d08bfda527a96f6efe41f29b0b0
locxter <54595101+locxter@users.noreply.github.com>
2022-09-18 14:19:33 +0200

    Renamed canvas to context.

--- src/main/java/com/github/locxter/twrfhn/lib/Visualisation.java
+++ src/main/java/com/github/locxter/twrfhn/lib/Visualisation.java
@@ -21,40 +21,40 @@ public class Visualisation extends JComponent {
 
     // Method to draw the component
     @Override
-    protected void paintComponent(Graphics canvas) {
+    protected void paintComponent(Graphics context) {
         int width = getWidth();
         int height = getHeight();
         // Clear the component
-        super.paintComponent(canvas);
-        canvas.setColor(new Color(255, 255, 255));
-        canvas.fillRect(0, 0, width, height);
+        super.paintComponent(context);
+        context.setColor(new Color(255, 255, 255));
+        context.fillRect(0, 0, width, height);
         // Calculate the scaling ratio and center the canvas
         if ((double) width / height > 16.0 / 9) {
             scalingRatio = height / 1080.0;
-            canvas.translate((int) Math.round((width - (scalingRatio * 1920)) / 2), 0);
+            context.translate((int) Math.round((width - (scalingRatio * 1920)) / 2), 0);
         } else {
             scalingRatio = width / 1920.0;
-            canvas.translate(0, (int) Math.round((height - (scalingRatio * 1080)) / 2));
+            context.translate(0, (int) Math.round((height - (scalingRatio * 1080)) / 2));
         }
         if (showStartScreen) {
             // Draw a start screen if no visualisation has been requested yet
             Font font = new Font(Font.SANS_SERIF, Font.PLAIN, getScaledValue(64));
-            FontMetrics metrics = canvas.getFontMetrics(font);
+            FontMetrics metrics = context.getFontMetrics(font);
             String message = "Press \"Calculate\" to see the visualisation.";
-            canvas.setFont(font);
-            canvas.setColor(new Color(0, 0, 0));
-            canvas.drawString(message, (int) Math.round(((double) getScaledValue(1920) - metrics.stringWidth(message)) / 2), (int) Math.round((((double) getScaledValue(1080) - metrics.getHeight()) / 2) + metrics.getAscent()));
+            context.setFont(font);
+            context.setColor(new Color(0, 0, 0));
+            context.drawString(message, (int) Math.round(((double) getScaledValue(1920) - metrics.stringWidth(message)) / 2), (int) Math.round((((double) getScaledValue(1080) - metrics.getHeight()) / 2) + metrics.getAscent()));
         } else {
             int diskCount = rods.get(0).size() + rods.get(1).size() + rods.get(2).size();
             for (int i = 0; i < 3; i++) {
                 // Drawing a rod
-                canvas.setColor(new Color(0, 0, 0));
-                canvas.fillRect(getScaledValue(304 + (640 * i)), 0, getScaledValue(32), getScaledValue(1080));
+                context.setColor(new Color(0, 0, 0));
+                context.fillRect(getScaledValue(304 + (640 * i)), 0, getScaledValue(32), getScaledValue(1080));
                 // Drawing the disks on the rod
-                canvas.setColor(new Color(0, 0, 255));
+                context.setColor(new Color(0, 0, 255));
                 for (int j = 0; j < rods.get(i).size(); j++) {
                     int diskWidth = (int) Math.round((640.0 / diskCount) * (rods.get(i).get(j) + 1));
-                    canvas.fillRect(getScaledValue(((320 - (diskWidth / 2)) + (640 * i))), getScaledValue((int) Math.round(1080 - (108 * (rods.get(i).size() - j)))), getScaledValue(diskWidth), getScaledValue(96));
+                    context.fillRect(getScaledValue(((320 - (diskWidth / 2)) + (640 * i))), getScaledValue((int) Math.round(1080 - (108 * (rods.get(i).size() - j)))), getScaledValue(diskWidth), getScaledValue(96));
                 }
             }
         }
