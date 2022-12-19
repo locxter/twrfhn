f3b426444e231eb36876e621a524828420aab8ab
locxter <54595101+locxter@users.noreply.github.com>
2022-12-19 15:01:03 +0100

    Fixed a typo.

--- src/test/java/com/github/locxter/twrfhn/MainTest.java
+++ src/test/java/com/github/locxter/twrfhn/MainTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 // Main test class
 public class MainTest {
-    // Required mehtod
+    // Required method
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
